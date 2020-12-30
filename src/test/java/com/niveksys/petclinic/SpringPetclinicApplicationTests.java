5ead9601e7d7574b2fe4d9a7c53c3dbaac7439d2
Kevin Tsang <niveksys@gmail.com>
2020-12-30 23:35:02 +0800

    Create project with Spring Initializr

---
+++ src/test/java/com/niveksys/petclinic/SpringPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niveksys.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
