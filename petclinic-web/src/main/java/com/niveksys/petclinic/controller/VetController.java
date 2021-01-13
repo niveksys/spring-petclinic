eb0d9220aa3fb82eee1555cd41bceb7f27e4725d
Kevin Tsang <niveksys@gmail.com>
2021-01-14 00:27:55 +0800

    Added Vets List with update on Controller. Closes #14.

--- petclinic-web/src/main/java/com/niveksys/petclinic/controller/VetController.java
+++ petclinic-web/src/main/java/com/niveksys/petclinic/controller/VetController.java
@@ -1,14 +1,25 @@
 package com.niveksys.petclinic.controller;
 
+import com.niveksys.petclinic.service.VetService;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({ "", "/", "/index", "/index.html" })
-    public String listVats() {
+    public String listVats(Model model) {
+        model.addAttribute("vets", this.vetService.findAll());
         return "vets/index";
     }
+
 }
