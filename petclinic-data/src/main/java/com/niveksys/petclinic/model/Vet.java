65e8588ff1fdb9adc673c90d7a2552ceb4b05a9e
Kevin Tsang <niveksys@gmail.com>
2021-01-15 21:50:46 +0800

    Added Vet Speciality Entity, and associated to Vet.  Closes #26.

--- petclinic-data/src/main/java/com/niveksys/petclinic/model/Vet.java
+++ petclinic-data/src/main/java/com/niveksys/petclinic/model/Vet.java
@@ -1,7 +1,18 @@
 package com.niveksys.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
     private static final long serialVersionUID = 1L;
 
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
