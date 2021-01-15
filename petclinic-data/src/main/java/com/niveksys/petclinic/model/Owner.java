5512cd97325fa2e1819b8ac6271cc25b9de53cac
Kevin Tsang <niveksys@gmail.com>
2021-01-15 21:48:01 +0800

    Added Visit, updated Owner with Pet relationship.  Closes #25.

--- petclinic-data/src/main/java/com/niveksys/petclinic/model/Owner.java
+++ petclinic-data/src/main/java/com/niveksys/petclinic/model/Owner.java
@@ -1,7 +1,18 @@
 package com.niveksys.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
     private static final long serialVersionUID = 1L;
 
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
