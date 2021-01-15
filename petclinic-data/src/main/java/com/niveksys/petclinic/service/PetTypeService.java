6b18719a5f1471028f1cf9dc4214af5469d5c290
Kevin Tsang <niveksys@gmail.com>
2021-01-15 22:29:22 +0800

    Added PetTypeMapService.  Closes #28.

---
+++ petclinic-data/src/main/java/com/niveksys/petclinic/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.niveksys.petclinic.service;
+
+import com.niveksys.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ petclinic-data/src/main/java/com/niveksys/petclinic/service/map/PetTypeMapService.java
@@ -0,0 +1,38 @@
+package com.niveksys.petclinic.service.map;
+
+import java.util.Set;
+
+import com.niveksys.petclinic.model.PetType;
+import com.niveksys.petclinic.service.PetTypeService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
