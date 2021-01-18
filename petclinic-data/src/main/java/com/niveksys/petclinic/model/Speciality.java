0e62674af4b8dc938ec556e1b4e40d501d3258ef
Kevin Tsang <niveksys@gmail.com>
2021-01-18 18:54:06 +0800

    Updated Vet to JPA entity.  Closes #36.

--- petclinic-data/src/main/java/com/niveksys/petclinic/model/Speciality.java
+++ petclinic-data/src/main/java/com/niveksys/petclinic/model/Speciality.java
@@ -1,9 +1,16 @@
 package com.niveksys.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- petclinic-data/src/main/java/com/niveksys/petclinic/model/Vet.java
+++ petclinic-data/src/main/java/com/niveksys/petclinic/model/Vet.java
@@ -3,10 +3,21 @@ package com.niveksys.petclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
 
     private static final long serialVersionUID = 1L;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specilaity", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialties = new HashSet<>();
 
     public Set<Speciality> getSpecialties() {
