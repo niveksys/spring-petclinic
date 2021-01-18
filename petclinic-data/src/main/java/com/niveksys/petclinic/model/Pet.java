daabc68325b6d181b556e6b1a721c9123795ba71
Kevin Tsang <niveksys@gmail.com>
2021-01-18 18:43:21 +0800

    Updated Owner as JPA entity.  Closes #35.

--- petclinic-data/src/main/java/com/niveksys/petclinic/model/Pet.java
+++ petclinic-data/src/main/java/com/niveksys/petclinic/model/Pet.java
@@ -2,12 +2,30 @@ package com.niveksys.petclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet")
 public class Pet extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
--- petclinic-data/src/main/java/com/niveksys/petclinic/model/PetType.java
+++ petclinic-data/src/main/java/com/niveksys/petclinic/model/PetType.java
@@ -1,8 +1,16 @@
 package com.niveksys.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet_type")
 public class PetType extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
