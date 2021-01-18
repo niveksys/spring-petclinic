2f5f07b2b054b1bda2084a6fa8e8b0040e6d5f42
Kevin Tsang <niveksys@gmail.com>
2021-01-19 00:23:04 +0800

    Added Visit Map Service.  Closes #45.

---
+++ petclinic-data/src/main/java/com/niveksys/petclinic/service/VisitService.java
@@ -0,0 +1,7 @@
+package com.niveksys.petclinic.service;
+
+import com.niveksys.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ petclinic-data/src/main/java/com/niveksys/petclinic/service/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package com.niveksys.petclinic.service.map;
+
+import java.util.Set;
+
+import com.niveksys.petclinic.model.Visit;
+import com.niveksys.petclinic.service.VisitService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null
+                || object.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
