9555f74fc758ea203711cf2694631671ed4c2d24
Kevin Tsang <niveksys@gmail.com>
2021-01-18 22:58:22 +0800

    Updated Visit as JPA Entity.  Closes #37.

--- petclinic-data/src/main/java/com/niveksys/petclinic/model/Pet.java
+++ petclinic-data/src/main/java/com/niveksys/petclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package com.niveksys.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -25,8 +29,11 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
 
-    @Column(name = "birth_date")
-    private LocalDate birthDate;
+    @Column(name = "birthday")
+    private LocalDate birthday;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -52,12 +59,19 @@ public class Pet extends BaseEntity {
         this.owner = owner;
     }
 
-    public LocalDate getBirthDate() {
-        return birthDate;
+    public LocalDate getBirthday() {
+        return birthday;
     }
 
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
     }
 
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 }
--- petclinic-data/src/main/java/com/niveksys/petclinic/model/Visit.java
+++ petclinic-data/src/main/java/com/niveksys/petclinic/model/Visit.java
@@ -2,12 +2,26 @@ package com.niveksys.petclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visit")
 public class Visit extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "descriptsion")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
