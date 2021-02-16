3e08bcd436ac5a37a7d853e7464f24c8cee95b13
Kevin Tsang <niveksys@gmail.com>
2021-02-16 10:57:09 +0800

    Added Implementation on Vets JSON Endpoint.  Closes #61.

--- petclinic-web/src/main/java/com/niveksys/petclinic/controller/VetController.java
+++ petclinic-web/src/main/java/com/niveksys/petclinic/controller/VetController.java
@@ -1,17 +1,20 @@
 package com.niveksys.petclinic.controller;
 
+import java.util.Set;
+
+import com.niveksys.petclinic.model.Vet;
 import com.niveksys.petclinic.service.VetService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-@RequestMapping("/vets")
 public class VetController {
 
     private final VetService vetService;
@@ -20,11 +23,16 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @GetMapping({ "", "/", "/index", "index.html" })
+    @GetMapping({ "/vets", "/vets/", "/vets/index", "/vets/index.html" })
     public String list(Model model) {
         log.debug("LIST all Vets.");
         model.addAttribute("vets", this.vetService.findAll());
         return "vets/list";
     }
 
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> listJson() {
+        log.debug("LIST all Vets (JSON).");
+        return vetService.findAll();
+    }
 }
